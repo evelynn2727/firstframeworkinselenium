2df8f187db1ebf618c5ddbb12f2c2b0c5205ea70
Ewelina Pałka <epalka@milosolutions.com>
2020-04-08 21:35:47 +0200

    Multithreading

--- src/test/java/configuration/LocalWebDriverProperties.java
+++ src/test/java/configuration/LocalWebDriverProperties.java
@@ -4,9 +4,6 @@ import driver.BrowserType;
 
 public class LocalWebDriverProperties {
     //metody zwracaja wlasciwosci dla poszczegolnych kluczy, analogicznie jak w przypadku appPtoperties
-    public static BrowserType getLocalBrowser(){
-        return BrowserType.valueOf(ConfigurationProperties.getProperties().getProperty("local.browser"));
-    }
 
     public static String getChromeWebDriverLocation(){
         return ConfigurationProperties.getProperties().getProperty("chrome.driver.location");
